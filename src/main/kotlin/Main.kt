8a30d0b0e2b9b1ab4c064cb533c0c687a52f016e
shallo-nairat <shallonairat@gmail.com>
2024-02-09 06:57:51 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val fullname:String = "Nairat Shallo"
+
+    val age:Byte = 22
+
+    val phoneNumber:String = "+254769333487"
+
+    val kg:Double = 42.5
+
+    val kenyan:Boolean = true
+
+    println(fullname)
+    println(age)
+    println(phoneNumber)
+    println(kg)
+    println(kenyan)
+}
+
